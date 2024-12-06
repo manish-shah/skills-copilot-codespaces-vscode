ef461566b192a181f20a93c339f9a4527d712337
Manish Shah <manish.shah.uk@gmail.com>
2024-12-06 14:47:12 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var skills = ["JavaScript", "React", "Node.js", "MongoDB", "Express"];
+    console.log("My skills:");
+    for (let i = 0; i < skills.length; i++) {
+        console.log(skills[i]);
+    }
+}
